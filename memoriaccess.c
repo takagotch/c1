0a4a9fd6a0ec7c4e424075b85d30ab5b3a81f3d8
takagotch <dyaccb@gmail.com>
2018-02-02 03:21:05 +0900

    vaid incFunc{ for (long i = 0; i < COUNT; ++i) ++counter; }

---
+++ memoriaccess.c
@@ -0,0 +1,26 @@
+long counter = 0;
+void incFunc(void) { for (long i = 0; i < COUNT; ++i) ++counter; }
+void incdecFunc(void) { for (long i = 0; i < COUNTl ++i) --counter; }
+
+int main(void)
+{
+  clock_t cl = clock();
+  thrd_t th1;
+  thrd_t th2;
+
+  if() != thrd_success
+    || thrd_create()
+  {
+    fprintf();
+    return -1;
+  }
+  thrd_join();
+  thrd_join();
+
+  printf();
+  printf();
+
+  return 0;
+}
+
+
