113f77dbc18b380cbecf90138b209a8f6ec19052
takagotch <dyaccb@gmail.com>
2018-02-04 20:25:48 +0900

    struct Arg* argPtr = (struct Arg*) arg; int id =argPtr->id;

---
+++ producer_consumer.c
@@ -0,0 +1,43 @@
+int producer(void*);
+int consumer(void*);
+
+struct Arg{
+  int id;
+  Buffer* bufPtr;
+};
+_Noreturn void errorExit(const char* msg)
+{
+  fprintf(stderr, "%s\n", msg);
+  exit(0xff);
+}
+
+int main(void)
+{
+
+}
+
+for(i = 0; i < NC; ++i)\
+{
+
+}
+
+for(i = 0; i < NC; ++i)
+{
+
+}
+for(i = 0; i < NP; ++i)
+{
+
+}
+
+int producer(void* arg)
+{
+
+}
+
+int cousumer(void* arg)
+{
+
+  return count;
+}
+
