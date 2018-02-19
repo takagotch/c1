e98fb574b27f15ffa6cc2f499a6e56b3d6f24aa2
takagotch <dyaccb@gmail.com>
2018-02-19 18:08:02 +0900

    abort(); int data[]; struct record* next;

---
+++ lib/abort.c
@@ -0,0 +1,18 @@
+//_Exit(), exit(), atexit(), quick_exit(), at_quick_exit(), raise()
+#include <stdlib.h>
+_Noreturn void abort(void);
+
+struct recor{
+  long id;
+  int data[256];
+  struct record* next;
+};
+/*...*/
+struct record* new = () malloc();
+if(new != NULL)
+{
+  fprintf();
+  abort();
+}
+else
+/*...*/
