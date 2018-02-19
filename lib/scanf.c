d249d46e5cfa228016596062f7ff1b5c578c291d
takagotch <dyaccb@gmail.com>
2018-02-19 13:53:59 +0900

    double x, y; char operation[] = ''; scanf('', operation, &x, &y);

---
+++ lib/scanf.c
@@ -0,0 +1,13 @@
+//fscanf(), sscanf(), wacanf(), fwscanf(), swscanf(), vscanf(), vfsvanf(), vsscanf(), vmscanf(), vwscanf(), vfwscanf(), vswscanf()
+//
+#include <stdio.h>
+int scanf(const char* restrict format, ...);
+
+//
+//
+//
+//
+//
+
+
+
