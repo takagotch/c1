eb42b72ee3aaf93e874fe85fb8f8031abfd30a33
takagotch <dyaccb@gmail.com>
2018-02-19 15:06:34 +0900

    int i =1; if(isupper(name[i])){} name[i] = tolower(name[i]);

---
+++ lib/setlocale.c
@@ -0,0 +1,38 @@
+//stroll(), strxfrm(), wcscoll(), wcsxfrm(), strftime(), wscftime()
+//
+#include <local.h>
+char* setlocale(int category, const char* locale_name);
+
+#define MAX_SRTING 80
+
+printf();
+char name[];
+fgets();
+name[] = '';
+
+printf();
+char locale[];
+fgets();
+locale[] = '';
+
+const char* newlocale = setlocale();
+if(newlocale == NULL)
+{
+
+}
+{
+
+}else{
+
+}
+name[] = toupper();
+
+int i = 1;
+if()
+{
+  name[i] = tolower(name[i]);
+  ++i;
+}
+printf();
+
+
