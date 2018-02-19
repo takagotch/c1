ffe3ec0c0f249247a6f974e133aa9536a70af78d
takagotch <dyaccb@gmail.com>
2018-02-19 12:50:35 +0900

    fprintf('%.24s: This file opened as stdout.\n', ctime(&sec));

---
+++ lib/freopen.c
@@ -0,0 +1,17 @@
+//freopen_s(), fopen(), fclose(), fflush(), setbuf()
+//
+#include <stdio.h>
+FILE* freopen();
+
+const char fname[] = "";
+if()
+{
+  fprintf(stderr, "");
+}
+else
+{
+  time_t sec;
+  time(&sec);
+  printf("", ctime(&sec));
+}
+
