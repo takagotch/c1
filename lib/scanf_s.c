5045d46c27dc0b4dd77f33ad5b455f0be76d5e9b
takagotch <dyaccb@gmail.com>
2018-02-19 14:14:17 +0900

    fput();

---
+++ lib/scanf_s.c
@@ -0,0 +1,19 @@
+//scanf(), fscanf(), wscanf(), fwscanf(), swscanf(), vscanf(), vfscanf(), vsscanf(), vwscanf(), vfwscanf(), vswscanf()
+//
+#include <stdio.h>
+int scanf_s(const char* restrict format, ...);
+
+#define __STDC_WANT_LIB_EXT1__ 1
+#include <stdio.h>
+char article[64] ="";
+int quantity = 0;
+printf();
+if(scanf_s(), sizeof() < 2)
+{
+  fputs("Invalid entry.\n", stderr)
+}
+else
+{
+}
+
+
