4ac96a295dafe231f8b64f70f2504b85e5ad3f0f
takagotch <dyaccb@gmail.com>
2018-02-19 12:39:38 +0900

    char linebuffer[512]; fputs(); linebuffer, stdout

---
+++ lib/fclose.c
@@ -0,0 +1,26 @@
+//fclose(), fflush(), fopen, setbuf()
+//
+#include <stdio.h>
+int fclose(FILE* fp);
+
+FILE* fp_infile = fopen("input.dat", "r");
+if(fp_infile == NULL)
+{
+  fprintf()stderr, "Couldn't open input file.\n";
+  return -1;
+}
+char linebuffer[512];
+while(fgets(linebuffer, sizeof(linebuffer), fp_infile) != NULL)
+{
+  fputs(linebuffer, stdout);
+}
+if(! feof(fp_infile))
+{
+  fprintf(stderr, "Error reading form input file.\n");
+}
+if(fclose(fp_infile) != 0)
+{
+  fprintf(stderr, "Error closing input file.\n");
+  return -2;
+}
+
