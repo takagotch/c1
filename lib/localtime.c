60ec4b03a7a234ad7b90b38eec471c615be71d45
takagotch <dyaccb@gmail.com>
2018-02-19 13:06:47 +0900

    cahr timeStr[26]; struct tm timeStruct = {0}; time_t now = {0}; time(&now); timeStruct.tm_year += 1;

---
+++ lib/localtime.c
@@ -0,0 +1,7 @@
+//gmtime()
+//asctime(), difftime(),gmtime(), lacaltime_s(), mktime(), strftime(), time()
+//
+#include <time.h>
+struct tm* localtime(const time_t* timer);
+
+
---
+++ lib/localtime_s.c
@@ -0,0 +1,23 @@
+//localtime(), gmtime(), gmtime_s(), strftime(), time()
+//
+#include <time.h>
+struct tm* localtime_s(const time_t* restrict timer, struct tm* restrict result);
+
+#define __STDC_WANT_LIB_EXT1__ 1
+#include <stdio.h>
+
+time_t now = {0};
+time(&now);
+
+struct tm timeStruct = {0};
+if(localtime_s(&timeStruct, &now) == 0)
+{
+  timeStruct.tm_year += 1;
+
+  char timeStr[26];
+  if(asctime_s(timeStr, sizeof(timeStr)), &timeStruct) == 0
+  {
+    printf("A year from today: %s", timeStr);
+  }
+}
+
