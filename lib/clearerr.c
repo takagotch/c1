3d6912be67bde87687bbee9ecc3b2a3d50ae6166
takagotch <dyaccb@gmail.com>
2018-02-20 17:55:11 +0900

    int c = fgetc(fp); fclose(fp); fprintf(stderr, ''); clearerr(fp); fprintf(stderr, '');

---
+++ lib/clearerr.c
@@ -0,0 +1,32 @@
+//feof(), ferror(), rewind()
+//
+#include <stdio.h>
+void clearerr(FILE* fp);
+
+FILE* fp = fopen("infile.dat", "r");
+if(fp == NULL)
+{
+  fprintf(stderr, "Couldn't open input file.\n");
+}
+else
+{
+  int c = fgetc(fp);
+  if(c == EOF)
+  {
+    if(feof(fp))
+    {
+      fprintf(stderr, "End of input file reached.\n");
+    }
+    else if(ferror(fp))
+    {
+      fprintf(stderr, "Error on reading from input file.\n");
+    }
+    clearerr(fp);
+  }
+  else
+  {
+    /*  */
+  }
+  fclose(fp);
+}
+
