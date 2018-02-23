70ac7148206a85937b25c20b379e3b03295cb128
takagotch <dyaccb@gmail.com>
2018-02-23 22:04:34 +0900

    FILE* fpTx, *fpRx, *fpLog; char rxbuffer[], *found; fgets(rxbuffer, sizeof(rxbuffer), fpRx);

---
+++ lib/strstr.c
@@ -0,0 +1,16 @@
+//strchr(), strpbrk(), wcsstr()
+//
+#include <string.h>
+char* strstr(const char* s1, cosnt char* s2)
+
+FILE* fpTx, fpRx, *fpLog;
+char rxbuffer[1024], *found;
+
+fgets(rxbuffer, sizeof(rxbuffer), fpRx);
+found = strstr(rxbuffer, "assword:");
+if(found != NULL)
+{
+  fputs("Got password prompt. Sending password", fpLog);
+  fputs("topsecret", fpTx);
+}
+
