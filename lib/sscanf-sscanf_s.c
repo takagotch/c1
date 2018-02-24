7cae373f2f558d7ffcf0437aac309610be023cc6
takagotch <dyaccb@gmail.com>
2018-02-25 02:15:57 +0900

    int sscanf(const char* restrict src, const char* restrict format ... ); int sscanf_s();

---
+++ lib/sscanf-sscanf_s.c
@@ -0,0 +1,11 @@
+//scanf(), fscanf(), vscanf(), vsfcanf(), wscanf(), fwscanf(), swscanf(), vswcanf(), vfwcanf(), vswcanf()
+//__STDC_LIB_EXT1_
+//scanf() 
+//
+
+#include<stdio.h>
+int sscanf(const char* restrict src, const char* restrict format, ... );
+int sscanf_s(const char* restrict src, const char* restrict format, ... );
+
+//fscanf(), strspan()
+
