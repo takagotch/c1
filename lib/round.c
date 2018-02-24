7bda51fb362c0404b30572fac63b17310a7d1def
takagotch <dyaccb@gmail.com>
2018-02-24 22:14:41 +0900

    double round(); float round(); long double roundl(); double x float x long double x

---
+++ lib/round.c
@@ -0,0 +1,8 @@
+//lround(), llround(), rint(), lrint(), llrint(), nearbyint(), nexttoward(), nextafter()
+//ceil(), trunc()
+//
+#include<math.h>
+double round(double x);
+float roundf(float x);
+long double roundl(long double x);
+
