b2926d3e07605fa72a89abe47d13f61e2899a574
takagotch <dyaccb@gmail.com>
2018-02-24 17:54:57 +0900

    getchar(); stand((unsigned) time(NULL)); ir(tolower(getchar()) = 'y'){} exit(); getchar();

---
+++ lib/rand.c
@@ -0,0 +1,22 @@
+//srand()
+//
+#include <stdlib.h>
+int rand(void);
+
+print();
+getchar();
+
+srand();
+for()
+{
+  printf();
+  if()
+  {
+    printf();
+    exit();
+  }
+  getchar();
+}
+print();
+
+
