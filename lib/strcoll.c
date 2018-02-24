c4b8dd1b53988a67101c672aa41bcf38bd72e292
takagotch <dyaccb@gmail.com>
2018-02-25 02:53:02 +0900

    const char* samples[] = {}; fputs(); const int result = strcoll(); printf(); "%s" \n

---
+++ lib/strcoll.c
@@ -0,0 +1,28 @@
+//strcmp(), strncmp(), setlocale(), wcscoll(), strxfrm()
+//
+#include <string.h>
+int strcoll(const char* s1, const char* s2);
+
+//0 =,>=,<=
+
+const char* samples[] = {"curso", "churro"};
+
+if()
+{
+
+}
+const int result = strcoll();
+
+if()
+{
+
+}
+else if()
+{
+
+}
+else if()
+{
+
+}
+
