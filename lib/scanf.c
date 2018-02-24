7fb373dd6d9b6d0d4351efdfddb056b7db1a9c8d
takagotch <dyaccb@gmail.com>
2018-02-24 22:31:16 +0900

    double x, y; char operation[16] = ; scanf(); , operation, &x, &y

--- lib/scanf.c
+++ lib/scanf.c
@@ -1,13 +1,23 @@
 //fscanf(), sscanf(), wacanf(), fwscanf(), swscanf(), vscanf(), vfsvanf(), vsscanf(), vmscanf(), vwscanf(), vfwscanf(), vswscanf()
+//__STDC_LIB_EXT1_
 //
 #include <stdio.h>
 int scanf(const char* restrict format, ...);
 
-//
-//
-//
-//
+//%[*][feild width][length modifier]specifier
+
+//hh d,i,o,u,x,X,n  signed char|unsigned char*
+//h  d,i,o,u,x,X,n  short  int*|unsigned short int* 
+//...
 //
 
+//d signed int* 
+//i signed int*
+//...
+//
+
+double x, y;
+char operation[16] = "";
+scanf("%15s%lf%*[^0123456789]%lf", operation, &x, &y);
 
 
