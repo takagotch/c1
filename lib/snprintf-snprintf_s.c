0fbec95941e835cba132377487f4508abee213c8
takagotch <dyaccb@gmail.com>
2018-02-25 01:08:08 +0900

    int snprintf(); char* restrict dest, size_t n, const char* restrict format, ...

---
+++ lib/snprintf-snprintf_s.c
@@ -0,0 +1,14 @@
+//printf(), fprintf(), sprintf(), vprintf(), vfprintf(), vsprintf(), vsnprintf()
+//wprintf(), fwprintf(), swprintf(), swprintf(), vwprint(), vfwprint(), vswprint()
+//__STDC_LIB_EXT1__
+//printf()
+//
+
+#include<stdio.h>
+int snprintf();
+int snprintf();
+
+char buffer[80];
+
+
+
