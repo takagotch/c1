dba8a61ecf466e4e0a197f8edd5d0611dd12723b
takagotch <dyaccb@gmail.com>
2018-02-25 00:05:05 +0900

    signal(SIGINT, sigint_handler); exit(); i = 0; int c = 0; while()P{ c = tolower() != '' && c != '' && c != EOF getchar()

--- lib/signal.c
+++ lib/signal.c
@@ -10,49 +10,61 @@ void (*signal(int sig, void(*handler)(int)))(int);
 //SIGTERM
 //
 
-#include<>
-#include<>
-#include<>
-#include<>
-#include<>
+#include<signal.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<stdint.h>
 
 static void sigint_handler(int sig);
 volatile sig_atomic_t i;
 
 int main(void)
 {
-  if()
+  if(signal(SIGINT, sigint_handler) == SIG_ERR)
   {
-  
+    perror("Failed to install SIGINT handler");
+    exit(3);
   }
 
-  while()
+  while(1)
   {
-  
+    puts("Press Ctrl+C to interrupt me.");
+    for(i = 0; i < SIG_ATOMIC_MAX ; i++)
+    {
+      if((i % 100000) == 0)
+      {
+        printf("\r%d", i / 1000000);
+	fflush(stdout);
+      }
+    }
+    raise(SIGNT);
   }
   return 0;
 }
 
 static void sigint_handler(int sig)
 {
-  if()
+  if(sig != SIGINT)
   {
-  
+    exit(1);
   }
-  signal();
-  puts();
+  signal(SIGINT, SIG_IGN);
+  puts("\nThis is the function siging_handler()."
+	"\nDo you want to exit the program now? [y/n]");
   int c = 0;
-  while()
+  while((c = tolower(getchar())) != 'y' && c != 'n' && c != EOF)
   {
-  
+    ;
   }
-  if()
+  if(c != 'n')
   {
-  
+    exit(0);
   }
   else
   {
+    i = 0;
   }
-  signal();
+  signal(SIGINT, sigint_handler);
 }
 
