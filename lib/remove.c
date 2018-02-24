b489bb5fcd4f7996ec7a946cb702ca2070ecb083
takagotch <dyaccb@gmail.com>
2018-02-24 21:44:03 +0900

    remove(); result = rename(fname_tmp, ); if()result{} fpritntf(); stderr, , errno tmpnam(); FILE* fp = fopen(); fp == NULL fprint(); return -1; fclose(); fp fputs(); int result = rename(); fname_tmp, ;

--- lib/remove.c
+++ lib/remove.c
@@ -1,12 +1,14 @@
 //fopen(), tmpfile()
+//
 #include <stdio.h>
 int remove(const char* filename);
 
-char fname_tmp[] = "";
+char fname_tmp[L-tmpnam] = "";
 FILE* fp = fopen(fname_tmp, "w+");
 if(fp == NULL)
 {
-
+  fprintf(stderr, "Couldn't open file \"%s\".\n", fname_tmp);
+  return -1;
 }
 
 fputs("hello...\n", fp);
@@ -16,11 +18,11 @@ fclose(fp);
 int result = rename(fname_tmp, "finished.txt");
 if(result)
 {
-  remove("");
-  result = rename();
+  remove("finished.txt");
+  result = rename(fname_tmp, "finished.txt");
   if(result)
   {
-    fprintf();
+    fprintf(stderr, "Error %d on trying to rename output file\n", errno);
   }
 }
 
