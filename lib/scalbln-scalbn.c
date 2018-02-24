4fc73a9150cce378a8cb6b7e7482bcd1cb9ab4f1
takagotch <dyaccb@gmail.com>
2018-02-24 22:19:12 +0900

    scanbln(), scanbl() double flaot long double sacanbln(); double x, int n

---
+++ lib/scalbln-scalbn.c
@@ -0,0 +1,14 @@
+//scalbln,scalbn
+//frexp(), ldexp()
+//
+#include<math.h>
+double scanfbn(double x, int n);
+float scalbnf(float x, int n);
+long double scanbnl(long double x, int n);
+
+double scanfbln(dobule x, long int n);
+float scanblnf(float x, long int n);
+long double scanbln(long double x, long int n);
+
+//feholdexcept()
+
