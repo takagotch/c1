cc2eb613a3b06e233ce06b610912dd6e2a789f84
takagotch <dyaccb@gmail.com>
2018-02-24 23:56:21 +0900

    printf(); for() i=0; i< SIG_ATOMIC_MAX;i++ i%100000 fflush(); stdoutl

---
+++ lib/signal.c
@@ -0,0 +1,58 @@
+//raise(), abort()
+//
+#include <signal.h>
+void (*signal(int sig, void(*handler)(int)))(int);
+
+//SIGPE
+//SIGLL
+//SIGSEGV
+//SIGINT
+//SIGTERM
+//
+
+#include<>
+#include<>
+#include<>
+#include<>
+#include<>
+
+static void sigint_handler(int sig);
+volatile sig_atomic_t i;
+
+int main(void)
+{
+  if()
+  {
+  
+  }
+
+  while()
+  {
+  
+  }
+  return 0;
+}
+
+static void sigint_handler(int sig)
+{
+  if()
+  {
+  
+  }
+  signal();
+  puts();
+  int c = 0;
+  while()
+  {
+  
+  }
+  if()
+  {
+  
+  }
+  else
+  {
+  }
+  signal();
+}
+
