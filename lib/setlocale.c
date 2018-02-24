991c4e0bd84525471533dad2b9ae8377cbc02439
takagotch <dyaccb@gmail.com>
2018-02-24 23:27:51 +0900

    name[0] = toupper(name[0]); int i = 1; isupper(name[i]){} name[] = tolower(); ++i;

--- lib/setlocale.c
+++ lib/setlocale.c
@@ -1,38 +1,52 @@
-//stroll(), strxfrm(), wcscoll(), wcsxfrm(), strftime(), wscftime()
+//is, isw
+//to, tow
+//stroll(), strxfrm(), wcscoll(), wcsxfrm()
+//strftime(), wscftime()
 //
 #include <local.h>
 char* setlocale(int category, const char* locale_name);
 
+//LC_ALL
+//LC_COLLATE stcoll(), strxfrm(), wcscoll(), wscxfrm()
+//LC_CTYPE   localconv()
+//LC_NUMERIC localeconv(), printf(), scanf(), strtod()
+//LC_TIME     strftime(), wcsftime()
+//
+
 #define MAX_SRTING 80
 
-printf();
-char name[];
-fgets();
-name[] = '';
+printf("Who are you? ");
+char name[MAX_STRING];
+fgets(name, sizeof(name), stdin);
+name[strlen(name) - 1] = '\0';
 
-printf();
-char locale[];
-fgets();
-locale[] = '';
+printf("What is your locale? ");
+char locale[MAX_STRING];
+fgets(locale, sizeof(locale), stdin);
+locale[strlen(locale) - 1] = '\0';
 
-const char* newlocale = setlocale();
+const char* newlocale = setlocale(LC_CTYPE, locale);
 if(newlocale == NULL)
 {
-
+  printf("Sorry, couldn't change the locale to %s.\n"
+	 "The current locale is %s. ",
+	 locale, setlocale(LC_CTYPE, NULL));
 }
+else
 {
-
-}else{
-
+  printf("The new locale is %s. ", newlocale);
 }
-name[] = toupper();
+name[0] = toupper(name[0]);
 
 int i = 1;
-if()
+if(isupper(name[i]))
 {
-  name[i] = tolower(name[i]);
-  ++i;
+  while(name[i] != '\0')
+  {
+    name[i] = tolower(name[i]);
+    ++i;
+  }
 }
-printf();
+printf("Hello there, %s!\n, name");
 
 
