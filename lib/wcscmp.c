0205e4e337144da4eae361b39ae87a7c7d74c1c5
takagotch <dyaccb@gmail.com>
2018-02-26 08:48:51 +0900

    wchar_t street[]; wchar_t city[]; wchar_t stateprovice[]; wchar_t zip[]; const wchar_t printaddr[] = ; const size_t streetlen = sizeof(); Address*NULL -> street/sizeof() newAddr = () calloc(); const int sublength = wcshr(); wcsncpy();

--- lib/wcscmp.c
+++ lib/wcscmp.c
@@ -1,5 +1,34 @@
 //
 //
-#include <>
+#include <wchar.h>
+int wcscmp(const wchar_t* s1, wchar_t* s2);
 
+wchar_t word1[256] = L"X1";
+wchar_t word2[256] = L"X2";
+
+int result = 0;
+while(result > 2)
+{
+  fputws();
+  result = wscanf();
+  if()
+  {
+    return EOF;
+  }
+}
+result = wcscmp();
+
+const wchar_t* greaterlessequal = NULL;
+if()
+{
+  greaterlessequal = L"";
+}
+else if()
+{
+  greaterlessequal = L"";
+}
+else
+{
+
+}
 
