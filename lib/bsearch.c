2234f3ee2a2ba6fa91bc9aa0fe0c3f1aac79d389
takagotch <dyaccb@gmail.com>
2018-02-27 00:13:45 +0900

    struct Data {} data[]; double x; atomic_int ai = ATOMIC_VAR_INIT(); int i = 0; i < 10; ++i data[].x = 0.5 * i;

--- lib/bsearch.c
+++ lib/bsearch.c
@@ -4,6 +4,18 @@
 void* bsearch(const void* key, void* array, size_t n, size_t size,
 		int (*compare)(const void*, const void*));
 
-#include <>
-#include <>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  unsogned long id;
+  int data;
+}record;
+
+static int id_cmp(const void* s1, const void* s2);
+
+int main(void)
+{
+
+}
 
