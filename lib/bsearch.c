532a85eb15d23a52bf0a87b466fe27cc9625b74b
takagotch <dyaccb@gmail.com>
2018-02-27 12:57:42 +0900

    struct Data {} data[]; double x; atomic_int ai = ATOMIC_VAR_INIT(); int i = 0; i < 10; ++i data[].x = 0.5 * i;

--- lib/bsearch.c
+++ lib/bsearch.c
@@ -43,7 +43,22 @@ int main(void)
 		    sizeof(record), id_cmp);
   if(found == NULL)
   {
-  
+    printf("No record with the ID %lu found.\n", querykey.id);
+  }
+  else
+  {
+    printf("The data value in record %lu is %d.\n",
+	  querykey.id, found->data);
+  }
+
+  static int id_cmp(const void* s1, const void* s2)
+  {
+    const record* p1 = (const record*) s1;
+    const record* p2 = (const record*) s2;
+
+    if(p1->id < p2->id)       { return -1; }
+    else if(p1->id == p2->id) { return 0;}
+    else                      { return 1;}
   }
 }
 
