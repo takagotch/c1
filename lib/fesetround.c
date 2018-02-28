bdf6d4c9824e1007d269f533ec4170a9d397f83a
takagotch <dyaccb@gmail.com>
2018-02-28 13:20:59 +0900

    const double roof_pitch = 20.0; const dobule printf Cosh()x %+.2f %+.1f

--- lib/fesetround.c
+++ lib/fesetround.c
@@ -1,8 +1,19 @@
-//
+//fegetround(), round(), lround(), llround(), nearbyint(), rint(), lrint(), llrint()
 
 #include <fenv.h>
 int fesetround(int round);
 
+//FE_DOWNWARD
+//FE_UPWARD
+//FE_TONEAREST
+//FE_TOWARDZERO
+
+#pragma STDC FENV_ACCESS ON
+int prev_rounding_dir = fegetround();
+int result = fesetround(FE_TOWARDZERO);
+
+fesetround(prev_rounding_dir);
+#pragma STDC FENV_ACCESS OFF
 
 
 
---
+++ lib/fetestexcept.c
@@ -0,0 +1,7 @@
+//feclearexcept(), feraiseexcept(), feholdexcept(), fesetexceptflag(), feupdatedenv(), fegetenv(), fesetenv()
+
+#include <fenv.h>
+int fetestexcept(int excepts);
+
+//feclearexcept(), feholdexcept()
+
