b75a82b39c52affe73d0b013d860661b1342cf8d
takagotch <dyaccb@gmail.com>
2018-02-28 20:34:07 +0900

    typedef{}record; long id; double value; record cur_rec = (){}; record OL, 0.0 const int reclength_file = sizeof(); record const long seek_id = 123L; FILE* fp = fopen();  fp == NULL perror(); fread(); &cur_rec.id, sizeof() long, fp break; cur_rec.id != seek_id

---
+++ lib/fwscanf-fwscanf_s.c
@@ -0,0 +1,15 @@
+//fwscanf, fwscanf_s
+
+//wcanf(), swscanf(), wctod(), wcstod(), wcstol(), wcstoul(), scanf(), fscanf()
+//fwprintf(), wprintf(), vfwprint(), vwprint()
+//__STDC_LIB__EXT1__
+
+#include <stdio.h>
+#include <wchar.h>
+int fwscanf(FILE* restrict fp, const wchar_t* restrict format, ... );
+int fwscnaf(FILE* restrict fp, const wchar_t* restrict format, ... );
+
+//wscanf()
+
+
+
