cde16ecfdcd90c6ec2112b462f7484fe5026765a
takagotch <dyaccb@gmail.com>
2018-02-28 18:26:36 +0900

    const double roof_pitch = 20.0; const dobule printf Cosh()x %+.2f %+.1f

--- lib/fopen_s.c
+++ lib/fopen_s.c
@@ -1,11 +1,19 @@
 //
-#include <>
-
+#include <stdio.h>
+errno_t fopen_s(FILE* restrict * restrict streamPtr,
+		const char* restrict name, const char* restrict mode);
 
 #define __STDC_WANT_LIB_EXT1__ 1
-#include <>
+#include <stdio.h>
 
 FILE* fp;
+const char filename[] = "new.txt";
 
+errno_t err = fopen_s(&fp, filename, "w+x");
+if(err != 0)
+{
+  fprintf(stderr, "Unable to create the file \"%s\".\n", filename);
+  exit(err);
+}
 
 
--- lib/fpclassify.c
+++ lib/fpclassify.c
@@ -1,15 +1,32 @@
-//
+//isfinite(), isinf(), isnan(), isnormal(), signbit()
 
 #include <math.h>
-
-
+int fpclassify(x);
 
 //FP_INFINITE
+//FP_NAN
+//FP_NORMAL
+//FP_SUBNORMAL
+//FP_ZERO
 
-
-double minimum()
+double minimum(double a, double b)
 {
+  register int aclass = fpclassify(a);
+  register int bclass = fpclassify(b);
 
+  if(aclass == FP_NAN || bclass == FP_NAN)
+  {
+    return NAN;
+  }
+  if(aclass == FP_INFINITE)
+  {
+    return (signbit(a) ? b : a); 
+  }
+  if(bclass == FP_INFINITE)
+  {
+    return (signbit(b) ? b : a)'
+  }
+  return ((a < b) ? a : b);
 }
 
 
