a815020bd6aa12702aa250253921750c70e776c7
takagotch <dyaccb@gmail.com>
2018-02-28 13:28:06 +0900

    const double roof_pitch = 20.0; const dobule printf Cosh()x %+.2f %+.1f

--- lib/fflush.c
+++ lib/fflush.c
@@ -11,7 +11,22 @@ int fflush(FILE* fp);
   const char filename[ ] = "once.txt";
 #endif /* FLUSH */
 
+FILE* fp = fopen(filename, "w");
+if(fp == NULL)
+{
+  fprintf();
+  return -1;
+}
+fputs();
+fflush();
 
+fputs();
+
+#ifdef FLUSH
+  fflush(fp);
+#endif
+
+raise();
 
 
 
