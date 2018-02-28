2839f5fe1c1b773a81c35f10e80e6e67b1dcc5fc
takagotch <dyaccb@gmail.com>
2018-02-28 18:18:32 +0900

    const double roof_pitch = 20.0; const dobule printf Cosh()x %+.2f %+.1f

--- lib/fopen.c
+++ lib/fopen.c
@@ -11,35 +11,37 @@ FILE* fopen(const char* restrict name, const char* restrict mode)'
 //"a+"
 
 
-if()
+if(argc != 3)
 {
-  fprintf();
+  fprintf(stderr, "Usage: program input-file output-file\n");
   exit(1);
 }
 //
-FILE* in = ();
-if()
+FILE* in = (strcmp(argv[i], "-") == 0) ? stdin : fopen(argv[i], "r");
+if(in == NULL)
 {
-
+  perror("Opening input file");
+  return -1;
 }
 
-//
-FILE* out = ();
-if()
+// "-" stdout
+FILE* out = (strcmp(argv[2], "-") == 0) ? stdout : fopen(argv[2], "a+");
+if(out == NULL)
 {
-
+  perror("Opening output file");
+  return -1;
 }
 int c = 0;
-while((c = fgetc()) != EOF)
+while((c = fgetc(in)) != EOF)
 {
-  if()
+  if(fputc(c, out) == EOF) 
   {
     break;
   }
 }
-if()
+if(! feof(in))
 {
-  perror();
+  perror("Error while copying");
 }
 fclose(in);
 fclose(out);
