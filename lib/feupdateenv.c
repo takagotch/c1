30cdf71e1c759d3d2567cd8a40d755f809290a0a
takagotch <dyaccb@gmail.com>
2018-02-28 13:24:02 +0900

    const double roof_pitch = 20.0; const dobule printf Cosh()x %+.2f %+.1f

---
+++ lib/feupdateenv.c
@@ -0,0 +1,7 @@
+//fegetexceptflag(), feraiseexcept(), feclearexcept(), fetestexcept(, fegetenv(), feholdexcept(), fesetenv(), feupdateenv()
+
+#include <fenv.h>
+void feupdateenv(const fenv_t* envp);
+
+//feholdexcept()
+
---
+++ lib/fflush.c
@@ -0,0 +1,21 @@
+//setbug(), setvbut()
+
+#include <stdio.h>
+int fflush(FILE* fp);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//cc -DEFUSH -o fflushtwice fflush.c
+//
+//
+
