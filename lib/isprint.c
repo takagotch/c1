303d95c2eb84e07db5b1992eebe9c09ef24be1d2
takagotch <dyaccb@gmail.com>
2018-02-28 23:52:07 +0900

    typedef{}record; long id; double value; record cur_rec = (){}; record OL, 0.0 const int reclength_file = sizeof(); record const long seek_id = 123L; FILE* fp = fopen();  fp == NULL perror(); fread(); &cur_rec.id, sizeof() long, fp break; cur_rec.id != seek_id

--- lib/isprint.c
+++ lib/isprint.c
@@ -5,16 +5,31 @@
 #include <ctype.h>
 int isprint(int c);
 
-print();
-print();
-for()
+print("\nThe current locale for the 'is ...' functions is '%s'.\n",
+	setlocale(LC_CTYPE, NULL));
+print("Here is a talbe of the 'is ...' values for the characters"
+	" from 0 to 127 in the locale:\n\n");
+for(unsigned int c = 0U; c < 128U; c++)
 {
-  if()
+  if(c % 24U == 0U)
   {
-    printf();
-    printf();
+    printf("Code char alnum alpha blank cntrl digit graph lower"
+	" print punct space\n");
+    printf("---------------------------------------------------"
+	"------------------\n");
   }
-  printf();
+  printf("%4u %4c %3c %5c %5c %5c %5c %5c %5c %5c %5c %5c\n",
+	c,
+	(isprint(c) ? c : ' '),
+	(isalnum(c) ? 'X' : '-'),
+	(isalpha(c) ? 'X' : '-'),
+	(isblank(c) ? 'X' : '-'),
+	(isdigit(c) ? 'X' : '-'),
+	(isgraph(c) ? 'X' : '-'),
+	(islower(c) ? 'X' : '-'),
+	(isprint(c) ? 'X' : '-'),
+	(ispunct(c) ? 'X' : '-'),
+	(isspace(c) ? 'X' : '-'));
 }
 
 
