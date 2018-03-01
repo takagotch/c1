ef56ab6b96339c546706f5eb0baca2429794baca
takagotch <dyaccb@gmail.com>
2018-03-01 18:02:39 +0900

    typedef{}record; long id; double value; record cur_rec = (){}; record OL, 0.0 const int reclength_file = sizeof(); record const long seek_id = 123L; FILE* fp = fopen();  fp == NULL perror(); fread(); &cur_rec.id, sizeof() long, fp break; cur_rec.id != seek_id

---
+++ lib/nexttoward.c
@@ -0,0 +1,11 @@
+//nextafter(), nearbyint(), rint(), lrint(), llrint(), round(), lround(), llround(), ceil(), floor()
+
+#include <math.h>
+double nexttoward();
+float nexttowardf();
+long double nexttowardl();
+
+float x = nextotwardf();
+printf();
+
+
