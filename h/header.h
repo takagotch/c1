50993ded42a9e65c2dd537738a87c6003290770b
takagotch <dyaccb@gmail.com>
2018-03-02 01:30:09 +0900

    struct Date birthday{}; static char strDate[]; sprintf(); return strDate; const char* dateAsString() struct Date d puts(); struct Date{}; unsingend int month : 4, _Bool isDST : 1;

---
+++ h/header.h
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+//threads.h
+thread_local //_Thread_local
+ONCE_FLAG_INT //once_flag
+TSS_DTOR_ITERATONS
+
+
+//time.h
+clock_t //clock() unsinged long
+time_t //timer() mktime() long
+struct tm //gmtime() localtime struct tm
+//errno_t,rsize_t stddef.h
+CLOCKS_PER_SEC //clock_t clock() CLOCKS_PER_SEC
+
+//uchar.h
+char16_t //unit_least16_t __STDC_UTF_16__ char16_t 
+char32_t //unit_least32_t __STDC_UTF_32__ char32_t
+//unit_least_t, unit_least32_t stdint.h uchar.h mbrtoc16(), c16rtomb(), mbrtoc32(), c32rtomb(), wcahr_t wcahr.h
+
+//wchar_t
+
+
+
