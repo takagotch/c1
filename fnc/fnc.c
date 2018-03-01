ee31cec4e064428d1f84faccfb4383e97eecf7d6
takagotch <dyaccb@gmail.com>
2018-03-02 01:40:44 +0900

    struct Date birthday{}; static char strDate[]; sprintf(); return strDate; const char* dateAsString() struct Date d puts(); struct Date{}; unsingend int month : 4, _Bool isDST : 1;

--- fnc/fnc.c
+++ fnc/fnc.c
@@ -1,6 +1,27 @@
 //io
 rename(), reomve()
 fopen(), freopen(), tmpfile(), fopen_s(), freopen_s(), tmpfile_s()
+fclose()
+tmpnam(), tmpnam_s()
+feof(), ferror(), clearer()
+ftell(), fgetpos()
+rewind(), fseek(), fsetpos()
+fflush()
+setbuf(), setvbuf()
 
+// stdio.h 			wcahr.h
+				fwide()
+fputc(),putc(), putchar()	fputwc(), putwc(), putwchar()
+fgetc(), getc(), getchar()	fgetwc(), getwc(), getchar()
+ungetc()			ungetwc()
+...
+
+
+
+//math
+//stdlib.h
+
+//stdio.h			stdint.h
+abs(), labs(), llabs()		inmabs()
 
 
