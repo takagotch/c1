b4d2f2899a62362e4d59192c88ef5a783848f2e6
takagotch <dyaccb@gmail.com>
2018-03-01 14:13:23 +0900

    typedef{}record; long id; double value; record cur_rec = (){}; record OL, 0.0 const int reclength_file = sizeof(); record const long seek_id = 123L; FILE* fp = fopen();  fp == NULL perror(); fread(); &cur_rec.id, sizeof() long, fp break; cur_rec.id != seek_id

---
+++ lib/mbstowcs.c
@@ -0,0 +1,9 @@
+//mbsrtowcs(), mbtowc(), wcstombs(), wcsrtombs()
+//__STDC_LIB_EXT1__
+
+#include <stdlib.h>
+size_t mbstowcs(wchar_t* restrict dest, const char* restrict src, size_t n);
+
+//localeconv()
+
+
