f7f2c8ccd33124d6155b33b46301b7bab92e8e17
takagotch <dyaccb@gmail.com>
2018-03-02 01:01:27 +0900

    struct Date birthday{}; static char strDate[]; sprintf(); return strDate; const char* dateAsString() struct Date d puts(); struct Date{}; unsingend int month : 4, _Bool isDST : 1;

---
+++ io/random.c
@@ -0,0 +1,19 @@
+//long ftell(FILE* fp);
+//int fgetpos(FILE* restrict fp, fpos_t* restrict ppos);
+
+#define ARRAY_LEN 1000
+long arrPos[ARRAY_LEN] = { OL };
+FILE* fp = fopen("messages.txt", "r");
+if(fp != NULL)
+{
+  int i = 0, c1 = '\n', c2;
+  while()
+  {
+    if()
+    {
+      arrPos[] = ftell() - 1;
+    }
+    c1 = c2;
+  }
+}
+
