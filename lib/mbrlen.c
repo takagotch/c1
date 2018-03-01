597fb8cf8a45dfbc6ed4265992f37e1f77ad3fc2
takagotch <dyaccb@gmail.com>
2018-03-01 13:33:02 +0900

    typedef{}record; long id; double value; record cur_rec = (){}; record OL, 0.0 const int reclength_file = sizeof(); record const long seek_id = 123L; FILE* fp = fopen();  fp == NULL perror(); fread(); &cur_rec.id, sizeof() long, fp break; cur_rec.id != seek_id

---
+++ lib/mbrlen.c
@@ -0,0 +1,15 @@
+//mblen(), mbrtowc()
+
+#include <stdlib.h>
+size_t mbrlen(const char* restrict s, size_t maxsize,
+	mbstate_t* restrict state);
+
+//+
+//0
+//(size_t)(-1)
+//(size_t)(-2)
+
+
+
+//mblen()
+
