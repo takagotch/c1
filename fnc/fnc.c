02d5f537b3a8f8caf9f626148907e6edd08e41db
takagotch <dyaccb@gmail.com>
2018-03-02 18:42:13 +0900

    struct Date birthday{}; static char strDate[]; sprintf(); return strDate; const char* dateAsString() struct Date d puts(); struct Date{}; unsingend int month : 4, _Bool isDST : 1;

--- fnc/fnc.c
+++ fnc/fnc.c
@@ -1,4 +1,5 @@
 //io
+//stdio.h
 rename(), reomve()
 fopen(), freopen(), tmpfile(), fopen_s(), freopen_s(), tmpfile_s()
 fclose()
@@ -9,19 +10,35 @@ rewind(), fseek(), fsetpos()
 fflush()
 setbuf(), setvbuf()
 
+//wchar_t
+//wchar.h
 // stdio.h 			wcahr.h
 				fwide()
 fputc(),putc(), putchar()	fputwc(), putwc(), putwchar()
 fgetc(), getc(), getchar()	fgetwc(), getwc(), getchar()
 ungetc()			ungetwc()
-...
+fputs(), puts()			fputws()
+fgets(), gets(), gets_s()	fgetws()
+fwrite()
+fread()
+printf(), vprintf(), vprintf(), sprintf(), vsptintf(), snprintf(), vsnprintf()
+				wprintf(), vwprintf(), fwprintf(), vfwprintf(), swprintf(), swprintf(), vswprintf()
+scanf(), vscnaf(), fscanf(), vfscanf(), sscanf(), vsscanf()
+				wscanf(), vwscanf(), fwscanf(), vfwcanf(), swscanf(), vswscanf()
 
 
 
 //math
 //stdlib.h
-
+//intmax_t imax inttypes.h
 //stdio.h			stdint.h
 abs(), labs(), llabs()		inmabs()
+div(), ldiv(), lldiv()		imaxdiv()
+rand(), srand()
 
+
+
+
+//math.h			stdint.h
+sin(),
 
