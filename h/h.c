5e43ebd887df079d9bf5faf3be6e2963619a1384
takagotch <dyaccb@gmail.com>
2018-03-02 20:52:08 +0900

    struct Date birthday{}; static char strDate[]; sprintf(); return strDate; const char* dateAsString() struct Date d puts(); struct Date{}; unsingend int month : 4, _Bool isDST : 1;

--- h/h.c
+++ h/h.c
@@ -42,6 +42,39 @@ c = toupper(c) //toupper()
 //errno strcpy sting.h
 //#include
 
+//__STDC_LIB_EXT1__
+//stdio.h scanf() scanf_s()
+//string.h strcpy() strcpy_s()
+//__STDC_WANT_LIB_EXT1__ 1
+#define __STDC__WANT_LIB_EXT1__ 1
+#include <stdio.h>
 
+//__STDC_LIB_EXT1__ 0
+//__STDC_LIB_EXT1__
+
+
+//rsize_t
+//stddef.h rsize_t RSIZE_MAX
+//abort() set_constraint_handler_s()
+//errno_t errno.h int
+
+//assert.h
+//assert() NODEBUG 
+//static_assert _Static_assert
+
+//complex.h
+////__STDC_NO_COMPLEX__
+//csin()
+//cerf(), cerfc(), cexpm1(), clog10(), clog1p()
+//clog2(), clgamma(), ctgamma()
+//float _Complex long double _Complex
+//complex _Complex
+//_Complex_I const float _Complex i
+//I _Complex_I _Imaginary_I
+//imaginary _Imaginary
+//_Imaginary_I const float _Imaginary _Imaginary_I
+//CMPLX, CMPXF, CMPLXL
+
+//
 
 
