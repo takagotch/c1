c84d9262083e996d78d358b577e83d0b84fb5faf
takagotch <dyaccb@gmail.com>
2018-03-03 17:10:12 +0900

    struct Date birthday{}; static char strDate[]; sprintf(); return strDate; const char* dateAsString() struct Date d puts(); struct Date{}; unsingend int month : 4, _Bool isDST : 1;

--- io/io.c
+++ io/io.c
@@ -158,12 +158,115 @@ typedef struct{
   long key;
   char name[32];
 } Record_t;
+char inFile[] = "records.dat";
+char outFile[] = "packed.dat";
+
+static inline void error_exit(int status, const char* error_msg)
+{
+  fputs(error_msg, stderr);
+  exit(status);
+}
+
+int main(void)
+{
+  FILE* fpIn = NULL;
+  FILE* fpOut = NULL;
+  Record_t* pArray = NULL;
+
+  if((fpIn = fopen(inFile, "rb")) == NULL)
+  {
+    error_exit(1, "Error on opening input file");
+  }
+  else if((fpOut = fopen(outFile, "wb")) == NULL)
+  {
+    error_exit(2, "Error on opening output file.");
+  }
+  else
+  {
+    if((pArray = malloc(ARRAY_LEN* sizeof(Record_t))) == NULL)
+    {
+      error_exit(3, "Insufficient memory.");
+    }
+  }
+  Record_t record = {0};
+  unsigned int i = OU;
+  while(fread(&record, sizeof(Record_t), 1, fpIn) == 1)
+  {
+    if(record.key != OL)
+    {
+      pArray[i++] = record;
+      if(i == ARRAY_LEN)
+      {
+        break;
+      }
+      i = OU;
+    }
+  }
+if(i > 0 && !ferror(fpOut))
+{
+  fwrite(pArray, sizeof(Record_t), i, fpOut);
+}
+if(ferror(fpOut))
+{
+  error_exit(4, "Error on writing to output file.");
+}
+else if(ferror(fpIn))
+{
+  error_exit(5, "Error on reading input file.");
+}
+return 0;
+
+}
 
 
 
+int printf(const char* restrict format, ...);
+int fprintf(FILE* restrict fp, const char* restrict format, ...);
+int sprintf(char* restrict buf, const char* restrict format, ...);
+int snprintf(char* restrict buf, const char* restrict format, ...);
 
 
+int vprintf(const char* restrictformat, va_list argptr);
+int vfprintf(FILE* restrict fp, const char* restrict format, va_list argptr);
+int vsprintf(char* restrict buf, const char* restrict format, va_list argptr);
+int vsnprintf(char* restrict buffer, size_t n, const char( restrict format, va_list argptr);
 
+
+int score = 120;
+char player[] = "Mary";
+printf("%s has %d points.\n", player, score);
+
+%[flags][field_with][.precision][length_modifier]specifier
+
+
+printf();
+printf();
+printf();
+printf();
+
+int month = 5, day = 1, year = 1987;
+printf();
+
+char str[] "";
+int width = 30;
+printf();
+
+//c int 
+//s
+
+char* team[] = {};
+char spatator = '';
+for()
+{
+  printf();
+}
+putchar();
+
+
+char msg[] = "";
+printf();
+printf();
+printf();
 
 
 
