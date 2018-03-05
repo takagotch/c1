538034cdc03b2092888a6924d988c962aa5ba49a
takagotch <dyaccb@gmail.com>
2018-03-05 20:37:22 +0900

    struct Date birthday{}; static char strDate[]; sprintf(); return strDate; const char* dateAsString() struct Date d puts(); struct Date{}; unsingend int month : 4, _Bool isDST : 1;

--- bs/bs.c
+++ bs/bs.c
@@ -188,3 +188,236 @@ ROW_t mat[] = {{0.0F, 0.1F},
 int nRows = sizeof(mat) /sizeof(ROW_t);
 printMatrix(mat, nRows);
 
+//func
+type name(parameter_declarations)
+{
+  //
+}
+
+
+//cylinderVolume()
+extern double cylinderVolume(double r, double h)
+{
+  const double pi = 3.1415926536;
+  return pi * r * r * h;
+}
+
+
+//printArray() flaot printf
+static void printArray(const float array[], int n)
+{
+  for(int i = 0; i < n; ++i)
+  {
+    printf("%12.2f", array[i]);
+    if(i % 5 == 4)
+    {
+      putchar('\n');
+    }
+  }
+  if(n % 5 != 0)
+  {
+    putchar('\n');
+  }
+}
+
+static void printArray(const float [], int);
+
+int main()
+{
+  float farray[123];
+  printArray(farray, 123);
+}
+
+
+double cylinderVolume(r, h)
+double r, h;
+{
+  const doulbe pi = 3.1415926536;
+  return pi * r * r * h;
+}
+
+//factorial()
+static long double factorial(register unsigned int n)
+{
+  long double f = 1.0L;
+  while(n > 1)
+  {
+    f *= n--;
+  }
+  return f;
+}
+
+
+type name[]
+type* name
+
+//addArray()
+static void addArray(register float a1[], register const float a2[], const int len)
+{
+  register float* end = a1 + len;
+  for(; a1 < end; ++a1, ++a2)
+  {
+    *a1 += *a2;
+  }
+}
+
+static void addArray(register float* a1, register const float* a2, int len)
+{
+  //
+}
+
+
+int func(long array[const static 5])
+{/**/}
+
+
+//maximum()
+dobule maximum(const int nrows, const int ncols, const double matrix[nrows][ncols])
+{
+  double max = matrix[0][0];
+  for(int r = 0; r < nrows; ++r)
+  {
+    for(int c = 0; c < ncols; ++c)
+    {
+      if(max < matrix[r][c])
+      {
+        max = matrix[r][c];
+      }
+    }
+  }
+  return max;
+}
+
+
+int main(void){}
+int main(int argc, char* argv[]){}
+int main(int argc, char* argv[], char* envp[]){}
+
+argc //argument count
+argv //argument vector
+envp //environment pointer
+
+
+
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+  if(argc == 0)
+  {
+    puts("No command line available.");
+  }
+  else
+  {
+    printf("The program now running: %s\n", argv[0]);
+    if(argc == 1)
+    {
+      puts("No arguments received on the command line.");
+    }
+    else
+    {
+      puts("The command-line arguments:");
+      for(int i = 1; i < argc; ++i)
+      {
+        puts(argv[i]);
+      }
+    }
+  }
+}
+
+//$ ./args one two "and three"
+
+
+int rename();
+extern int rename();
+int rename(const oldname, const char* newname);
+int rename(const char*, const char*);
+int printf(const char* format, ...);
+
+double maximum(int nrows, int ncols, double matrix[nrows][ncols]);
+double maximum(int nrows, int ncols, double matrix[][ncols]);
+double maximum(int nrows, int ncols, double matrix[*][*]);
+double maximum(int nrows, int ncols, double matrix[][*]);
+
+maximum(r, c, mat);
+
+int var;
+scanf("%d", &var);
+
+#include <string.h>
+struct Node{
+  long key;
+  char name[32];
+  struct Node* next;
+};
+void initNode(struct Node* pNode)
+{
+  memset(pNode, 0, sizeof(*pNode));
+  strcpy(pNode->name, "XXXX");
+}
+
+oid printNode (const struct Node* pNode);
+{
+  printf("Key: %ld\n", pNode->key);
+  printf("Name: %s\n", pNode->name);
+}
+
+
+
+#include <stdlib.h>
+struct Node* mkNode()
+{
+  struct Node* pNode = malloc(sizeof(struct Node));
+  if(pNode != NULL)
+  {
+    initNode(pNode);
+  }
+  return pNode;
+}
+
+
+//swapf()
+inline void swapf(float* p1, flaot* p2)
+{
+  const flaot tmp = *p1;
+  *p1 = *p2;
+  *p2 = tmp;
+}
+void selection_sortf(float a[], int n)
+{
+  for(register int i = 0; i < n - 1; ++i)
+  {
+    register int mini = i;
+    for(registre int j = i + 1; j < n; ++j)
+    {
+      if(a[j] < a[mini])
+      {
+        mini = j;
+      }
+    }
+    swapf(a + i, a + mini);
+  }
+}
+
+
+extern void swapf(float* p1, float* p2);
+
+
+_Noreturn void myAbort()
+{
+  abort();
+}
+
+//binarySearch()
+const long* binarySearch()
+{
+  const int m = n / 2;
+  if() {}
+  if() {}
+  if() {}
+  else {}
+}
+
+
+
